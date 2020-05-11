50827291bfde348fac1d75039ca40faac5acd314
blakeward084 <58792812+blakeward084@users.noreply.github.com>
2020-05-10 17:21:25 -0700

    Add files via upload

---
+++ EmployeeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcEmployees.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Employee ID")]
+        public int EmployeeID { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
